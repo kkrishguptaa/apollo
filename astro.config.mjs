ad1552335750ecf59fbee7647d49798f75f5a3df
Krish Gupta <m.krishggupta@icloud.com>
2025-09-20 23:49:56 +0530

    feat: Meta Tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,36 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://blog.krishg.com",
   integrations: [mdx(), sitemap()],
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Space Grotesk",
+        cssVariable: "--font-space-grotesk",
+        fallbacks: ["Montserrat", "Poppins", "sans-serif"],
+        weights: ["400", "500", "600", "700", "900"],
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Lexend",
+        cssVariable: "--font-lexend",
+        fallbacks: ["Inter", "Arial", "sans-serif"],
+        weights: ["400", "500", "600", "700"],
+      },
+    ],
+    contentIntellisense: true,
   },
 });
