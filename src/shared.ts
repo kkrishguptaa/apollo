ab3319bc589ac909b86abd83b3668a02f54d98e5
Krish Gupta <m.krishggupta@icloud.com>
2025-09-28 19:10:19 +0530

    feat: use common site definition

--- src/shared.ts
+++ src/shared.ts
@@ -1,8 +1,10 @@
+import { site as astroSite, base } from 'astro:config/server'
+
 export const title = "Krish Gupta's Column";
 export const description =
   'The Home of Krish Gupta on the Internet. Articles on Web Development, Programming, Tech and more.';
 export const topics = ['Full Stack Development', 'DevOps', 'Frontend'];
-export const site = new URL('https://blog.krishg.com');
+export const site = new URL(base, astroSite) || new URL('https://blog.krishg.com')
 
 export const config = {
   blogs: {
